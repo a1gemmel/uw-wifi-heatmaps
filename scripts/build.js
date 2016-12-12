326dfeaf0d567af4e411f578f5b658643bb272e5
Andrew Gemmel <andrew.gemmel@live.ca>
2016-12-11 22:29:40 -0500

    add isttoosm inversion to build process

--- scripts/build.js
+++ scripts/build.js
@@ -7,25 +7,14 @@ const getWifiData = require("./scraper");
 fs.copySync("src", "public");
 
 // copy data to server root for playing around with
-fs.copySync("data", "public");
+fs.copySync("data/UW_Buildings.geojson", "public/UW_Buildings.geojson");
 
 // Transform geoJSON object into the minimum data span we need for displaying
 // ~Optimize the payload we send to the browser~
-const obj = JSON.parse(fs.readFileSync("data/buildings.geojson", "utf8"));
-
-function formatBuilding(building) {
-	return {
-		name: building.properties.name,
-		alt_name: building.properties.alt_name,
-		coordinates: building.geometry.coordinates
-	};
-}
-
-const buildings = _.map(obj.features, formatBuilding);
+const obj = JSON.parse(fs.readFileSync("data/ISTtoOSM.json", "utf8"));
 
 // output formatted building data to server root
-fs.appendFile("public/buildings.json", JSON.stringify(buildings, null, 2));
-
+fs.appendFile("public/OSMtoIST.json", JSON.stringify(_.invert(obj), null, 2));
 
 const url = "https://uwaterloo.ca/information-systems-technology/statistics/wifi-charts/building-select-index";
 
